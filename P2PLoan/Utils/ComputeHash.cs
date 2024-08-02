b48fd0f1f12d0694e7d31a3a8be47ea15309db96
Okikiola Osunronbi <osunronbiolanlesi@gmail.com>
2024-08-02 10:30:42 +0100

    remove dummy secret from compute hash util class

--- P2PLoan/Utils/ComputeHash.cs
+++ P2PLoan/Utils/ComputeHash.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.Extensions.Configuration;
 
 namespace P2PLoan.Utils;
 
 public static class ComputeHash
 {
-    private const string DEFAULT_MERCHANT_CLIENT_SECRET = "91MUDL9N6U3BQRXBQ2PJ9M0PW4J22M1Y";
-
-    public static string Compute(string requestBody)
+    public static string Compute(string requestBody, string clientSecret)
     {
-        using (var hmac = new HMACSHA512(Encoding.UTF8.GetBytes(DEFAULT_MERCHANT_CLIENT_SECRET)))
+        using (var hmac = new HMACSHA512(Encoding.UTF8.GetBytes(clientSecret)))
         {
             byte[] hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(requestBody));
             return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
